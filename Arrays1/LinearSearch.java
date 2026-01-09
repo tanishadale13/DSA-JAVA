306f093f56c4c5e88f0a97c7cdf3396d0cfc69a2
tanishadale13 <tanishadale11@gmail.com>
2026-01-09 13:12:11 +0530

    Create LinearSearch.java
    Linear Search  Algorithm

---
+++ Arrays1/LinearSearch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LinearSearch{
+    public static int LS(int num[],int key){
+        for(int i=0; i<num.length; i++){
+            if(num[i]==key){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String args[]){
+        int num[]={2,4,6,8,10,12,14,16};
+        int key=10;
+        int index=LS(num,key);
+        if(index==-1){
+            System.out.println("Not Found");
+        }
+        else{
+            System.out.println("Key is at index: "+index);
+        }
+    }
+}
